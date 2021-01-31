e75c1fb7e34881eb35e67778c1d736218f277cf0
mr19331 <mr19331@gmail.com>
2021-01-31 18:30:10 +0000

    cart context and delivery logic added

--- pet_hub/settings.py
+++ pet_hub/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # rqd by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart-contexts.cart_contents',
             ],
         },
     },
@@ -158,3 +159,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 30
+STANDARD_DELIVERY_PERCENTAGE = 10
