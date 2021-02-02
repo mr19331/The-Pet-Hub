f5cafe583ddb2a764d9b2a288900932764ac6498
mr19331 <mr19331@gmail.com>
2021-02-02 10:35:18 +0000

    toast notifications added

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -11,6 +14,8 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     """ Add quantity of product to the shopping cart"""
+
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -31,6 +36,7 @@ def add_to_cart(request, item_id):
             cart[item_id] += quantity
         else:
             cart[item_id] = quantity
+            messages.success(request, f'You have added { product.name} to your cart ')
 
     request.session['cart'] = cart
 
