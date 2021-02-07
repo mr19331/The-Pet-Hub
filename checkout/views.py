0acdc6bd776e9ddf7a4dbc4f135a0428cf88e313
mr19331 <mr19331@gmail.com>
2021-02-07 14:32:18 +0000

    finished adding checkout success logic

--- checkout/views.py
+++ checkout/views.py
@@ -19,14 +19,14 @@ def checkout(request):
 
         form_data = {
             'full_name': request.POST['full_name'],
-            'email': request.Post['email'],
-            'phone_number': request.Post['phone_number'],
-            'country': request.Post['country'],
-            'postcode': request.Post['postcode'],
-            'town_or_city': request.Post['town_or_city'],
-            'street_address1': request.Post['street_address1'],
-            'street_address2': request.Post['street_address2'],
-            'county': request.Post['county'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
