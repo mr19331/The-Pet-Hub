9b1742629cc2b74a2143b19b80199940863cffb1
mr19331 <mr19331@gmail.com>
2021-02-12 10:03:12 +0000

    add blog template for post display

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from django.views import generic 
+from django.views import generic
 from .models import Post
 
 
-class PostList(generic.ListView):
-    queryset = Post.objects.order_by('-created_on')
-    template_name = 'blog.html'
+def PostList(request):
 
-
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
+    template = 'blog/blog.html'
+    return render(request, template)
