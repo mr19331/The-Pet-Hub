f9baa56e06f764bb26a08761f9c980bc0adfefc6
mr19331 <mr19331@gmail.com>
2021-02-19 13:25:57 +0000

    added blog comment model and views urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList, name='blog'),
+    path('', views.PostList.as_view(), name='blog'),
+    # path ('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail')
     ]
