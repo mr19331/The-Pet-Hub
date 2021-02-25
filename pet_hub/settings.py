ded7e0d7f721d2fae19c4223fd0756c4beeae0ea
mr19331 <mr19331@gmail.com>
2021-02-25 16:14:35 +0000

    removed secret key and set debug

--- pet_hub/settings.py
+++ pet_hub/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zg$49=^a84lru=w#!sd0zq3oqhw=&p4tuu*=yx27iz(z)_uw$m'
+SECRET_KEY = os.environ.get('SECRET_KEY', '') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-pet-hub.herokuapp.com', 'localhost']
 
