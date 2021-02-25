a2741140dd0821ed8607bb10ae94c7ffa32db85a
mr19331 <mr19331@gmail.com>
2021-02-25 15:51:25 +0000

    added gunicorn, Procfile and disabled collectstatic

--- pet_hub/settings.py
+++ pet_hub/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'zg$49=^a84lru=w#!sd0zq3oqhw=&p4tuu*=yx27iz(z)_uw$m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-pet-hub.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -127,18 +127,18 @@ WSGI_APPLICATION = 'pet_hub.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-
-# DATABASES = {
-#     'default': dj_database_url.parse('')
-
-# }
 
 
 # Password validation
